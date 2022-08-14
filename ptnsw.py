698fa71c6f442a752be08132ff8a596602876786
Jin_Tay <jtay0051@student.monash.edu>
2022-08-14 21:19:36 +1000

    trying to grasp socket.io

--- ptnsw.py
+++ ptnsw.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO,  emit
+from flask_socketio import SocketIO, send
 import requests 
 from google.transit import gtfs_realtime_pb2
 from requests.structures import CaseInsensitiveDict
@@ -9,41 +9,43 @@ from requests.structures import CaseInsensitiveDict
 
 
 #API access
-headers=CaseInsensitiveDict()
-headers['Authorization']="apikey 20ZUil4YfBkkGED8NegjB65z0ALHC3O5uMB0"
-url="https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/lightrail/cbdandsoutheast"
-resp = requests.get(url,headers=headers)
-responsebody=resp.content
 
-#Converting protobuf format into object for manipulation 
-feed = gtfs_realtime_pb2.FeedMessage()
-feed.ParseFromString(responsebody)
+
 #print(feed.entity) 
 
 #Retrieving coordinates 
-latitude=[]
-longitude=[]
-for entity in feed.entity:
-   latitude.append(entity.vehicle.position.latitude)
-   longitude.append(entity.vehicle.position.longitude)
 
+headers=CaseInsensitiveDict()
+headers['Authorization']="apikey PtFM8NeyUIGV6RS5hwTWOGiyC2IINOTtHxZz"
+url="https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/lightrail/cbdandsoutheast"
 
 app = Flask(__name__)
 socketio = SocketIO(app) 
 
 
-@app.route("/", methods=['POST', 'GET'])
 
-def senddata():
+@socketio.on('data')
+def event():
+   
+    resp = requests.get(url,headers=headers)
+    responsebody=resp.content
+
+#Converting protobuf format into object for manipulation 
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(responsebody)
+    latitude=[]
+    longitude=[]
+    for entity in feed.entity:
+        latitude.append(entity.vehicle.position.latitude)
+        longitude.append(entity.vehicle.position.longitude)
     coordinates=[latitude,longitude]
-  
+    send(coordinates)
 
-    return render_template('index.html',coordinates=coordinates) 
 
-@socketio.event
-def my_event(message):
-    emit('my response', {'data': 'got it!'})
 
+@app.route('/')
+def route():
+    return render_template('index.html')
 if __name__ == "__main__":
     socketio.run(app)
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,23 @@
 let map;
-coord_temp= document.getElementById('data').innerHTML
-coordinates=JSON.parse(coord_temp)
-console.log(coordinates)
-let latitude=coordinates[0]
-let longitude=coordinates[1]
+//coord_temp= document.getElementById('data').innerHTML
+//coordinates=JSON.parse(coord_temp)
+//console.log(coordinates)
+//let latitude=coordinates[0]
+//let longitude=coordinates[1]
 function initMap() {
-  let latitude=coordinates[0]
- let longitude=coordinates[1]
+  //let latitude=coordinates[0]
+ //let longitude=coordinates[1]
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:-33.8836784362793 , lng: 151.20750427246094},
     zoom: 14
   });
-  for(let i=0;i<latitude.length;i++){
+  /*for(let i=0;i<latitude.length;i++){
     new google.maps.Marker({
       position: {lat: latitude[i], lng: longitude[i]},
       map: map,
       title: '{[i]}'
     });
-  }
+  }*/
 
   
 }
