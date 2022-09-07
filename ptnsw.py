c612605b65414f3806667aaf9f387e17d431b64a
Jin_Tay <jtay0051@student.monash.edu>
2022-09-07 13:51:58 +1000

    documentation

--- ptnsw.py
+++ ptnsw.py
@@ -4,22 +4,16 @@ import requests
 from google.transit import gtfs_realtime_pb2
 from requests.structures import CaseInsensitiveDict
 import eventlet
-import time 
-
+#Server with API data
 
 
 
 #API access
-
-
-#print(feed.entity) 
-
-#Retrieving coordinates 
-
 headers=CaseInsensitiveDict()
 headers['Authorization']="apikey PtFM8NeyUIGV6RS5hwTWOGiyC2IINOTtHxZz"
 url="https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/lightrail/cbdandsoutheast"
 
+#App settings 
 app = Flask(__name__)
 app.config['SECRET KEY']='mysecret'
 socketio =  SocketIO(app,cors_allowed_origins="*")
@@ -27,44 +21,45 @@ app.debug = True
 
 #SocketIO(app, logger=True, engineio_logger=True, policy_server=False, async_mode='eventlet', manage_session=False, cors_allowed_origins="*")
 
+#Extracting tram data from API
 @socketio.on('spawn')
 def event():
     
     
-   
+    #Initialising variables 
     coordinates=[]
     latitude=[]
     longitude=[]
+    #Making the request
     resp = requests.get(url,headers=headers)
     responsebody=resp.content
 
     #Converting protobuf format into object for manipulation 
     feed = gtfs_realtime_pb2.FeedMessage()
     feed.ParseFromString(responsebody)
-                
+
+    #Looping to get each vehicles co-ordinates           
     for entity in feed.entity:
         latitude.append(entity.vehicle.position.latitude)
         longitude.append(entity.vehicle.position.longitude)
     coordinates=[latitude,longitude]
-                
+
+    #Sending data to client(index.html)            
     emit('tram',coordinates)
             
         
-
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data )
     
     
 
-
+#Connecting flask code with html 
 @app.route('/')
 def route():
     return render_template('index.html')
+#Creating local server 
 if __name__ == "__main__":
     eventlet.monkey_patch()
     eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app)
-    #socketio.run(app) 
+   
 
 
 
