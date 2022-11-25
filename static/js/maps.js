49d7ba50b1a6a1cc518f7882d0a34bae9f012f1b
Jin_Tay <jtay0051@student.monash.edu>
2022-11-25 16:26:47 +1100

    3d version

---
+++ static/js/maps.js
@@ -0,0 +1,16 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianRheTAwNTEiLCJhIjoiY2xhdjBnZXVtMDEzejNubG1nZ21hN3VjMiJ9.H7P1__12c75cLxmNEj6Zug';
+let origin=[151.20750427246094,-33.8836784362793,0]
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: origin, // starting position [lng, lat]
+    zoom: 16 ,// starting zoom
+    pitch: 60,
+antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased,
+});
+
+
+
+//[-33.8836784362793 , 151.20750427246094],
+
--- static/js/update.js
+++ static/js/update.js
@@ -1,6 +1,7 @@
 //Initialising socket.IO server
 let socket = io('http://localhost:5000');
 
+mapboxgl.accessToken = 'pk.eyJ1IjoianRheTAwNTEiLCJhIjoiY2xhdjBnZXVtMDEzejNubG1nZ21hN3VjMiJ9.H7P1__12c75cLxmNEj6Zug';
 
 //Icons
 const tramicons= 'https://img.icons8.com/cotton/30/000000/tram--v6.png'
@@ -10,17 +11,29 @@ const lightrail='https://e7.pngegg.com/pngimages/722/807/png-clipart-tram-light-
 
 
 //Initial variables 
-  let iconlist=[tramicons,tramicons,trainicon]
+  let iconlist=[tramicons,tramicons]
   let markers=[]
-
-//First iteration
-  //From emitting event (spawn), api data is extracted from api call and triggers (tram) event which sends the data to the index.html side
-  socket.emit('spawn')//Activating API call from ptnsw.py
+  let model=[]
+  let coord=[]
+  window.tb = new Threebox(
+    map,
+    map.getCanvas().getContext('webgl'), //get the context from the map canvas
+    { defaultLights: true }
+  );
+  map.on('style.load', function() {
+  map.addLayer({
+    id: 'custom_layer',
+    type: 'custom',
+    renderingMode: '3d',
+    onAdd: function (map, gl) {
+      socket.emit('spawn')//Activating API call from ptnsw.py
   socket.once('data',function(data){
     //Looping each type of vehicle
     for(let i=0;i<data.length;i++){
     //Initial variable for that vehicle 
     let markerstemp=[]
+    let modeltemp=[]
+    let coordtemp=[]
     //Extracting coordinate and route data
     let latitude=data[i][0]
     let longitude=data[i][1]
@@ -29,22 +42,42 @@ const lightrail='https://e7.pngegg.com/pngimages/722/807/png-clipart-tram-light-
       for(let j=0;j<routeid.length;j++){
         //Converting coordinates into google compatible coordinates
         let myLngLat=new mapboxgl.LngLat(longitude[j],latitude[j])
-        //Creating info window(DO THIS PART)
+        var geometry = new THREE.BoxGeometry(30, 30, 30);
+      let cube = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({ color: 0x660000 }));
+      cube = tb.Object3D({ obj: cube, units: 'meters' });
+      cube.setCoords([longitude[j],latitude[j]]);
+      tb.add(cube);
+
+       
         
-        //Creating the marker
-        let marker=new mapboxgl.Marker({})
-        marker.setLngLat(myLngLat)
-        marker.addTo(map)
+       
+       
       //Pushing marker in temporary array to segregate vehicle types   
-      markerstemp.push(marker) 
+      
+      modeltemp.push(cube)
+      coordtemp.push([longitude[j],latitude[j]])
+      
       }
       //Pushing array in overall marker array
-      markers.push(markerstemp)
+      
+      model.push(modeltemp)
+      coord.push(coordtemp)
     } 
+    console.log(model)
   })
+      
+    },
+    render: function (gl, matrix) {
+      tb.update(); //update Threebox scene
+    }
+  });
+})
+//First iteration
+  //From emitting event (spawn), api data is extracted from api call and triggers (tram) event which sends the data to the index.html side
+  
 
   //Updating function 
-  function replaceMarkers(markers){
+  function replaceMarkers(coord,model){
     //Repeating api data retrieval process by doing the same event calling 
     socket.emit('spawn')//Activating API call from ptnsw.py
     socket.on('data',function(data){//From emitting event (spawn) event (tram) is triggered) 
@@ -59,13 +92,19 @@ const lightrail='https://e7.pngegg.com/pngimages/722/807/png-clipart-tram-light-
         for(let j=0;j<latitude.length;j++){//Creating all the markers
             //Converting coordinates into google compatible coordinates 
             let myLngLat=new mapboxgl.LngLat(longitude[j],latitude[j])
-            //Extracting vehicle type array from the master marker array
-             let markersub=markers[i]
-             //preLng=markersub[j].getLngLat().lng 
-             //preLat=markersub[j].getLngLat().lat
-            //Setting and changing to the new position of that vehicle
-            
-             markersub[j].setLngLat(myLngLat)
+             
+             vehicle=model[i][j]
+             origin=coord[i][j]
+             destination=[longitude[j],latitude[j]]
+             /*if(destination==undefined){
+              destination=origin
+              travelPath(origin,destination,vehicle) 
+             }else{
+             travelPath(origin,destination,vehicle) 
+             }*/
+             model[i][j].setCoords([longitude[j],latitude[j]])
+             tb.update()
+             
             
             //requestAnimationFrame(markersub[j].setLngLat(myLngLat));
              } 
@@ -74,11 +113,53 @@ const lightrail='https://e7.pngegg.com/pngimages/722/807/png-clipart-tram-light-
     })
   }
 
+  function travelPath(origin,destination,vehicle){
+
+    // request directions. See https://docs.mapbox.com/api/navigation/#directions for details
+
+    var url = "https://api.mapbox.com/directions/v5/mapbox/driving/"+[origin, destination].join(';')+"?geometries=geojson&access_token=" + mapboxgl.accessToken
+
+
+    fetchFunction(url, function(data){
+
+      // extract path geometry from callback geojson, and set duration of travel
+      var options = {
+        path: data.routes[0].geometry.coordinates,
+        duration: 2000
+      }
+
+      // start the truck animation with above options, and remove the line when animation ends
+      vehicle.followPath(
+        options,
+      );
+
+      // set destination as the new origin, for the next trip
+      tb.update();
+
+    })
+  }
+  
+  //convenience function for fetch
+
+  function fetchFunction(url, cb) {
+    fetch(url)
+      .then(
+        function(response){
+          if (response.status === 200) {
+            response.json()
+              .then(function(data){
+                data//cb(data) works for like a second before going haywire
+              })
+          }
+        }
+      )
+  }
+
   //Repeating the update function for continous updates 
   try{
-  setInterval(replaceMarkers,2000,markers)
+  setInterval(replaceMarkers,5000,coord,model)
   }
 catch(e){
   console.log(e)
- setInterval(replaceMarkers,2000,markers)
+ setInterval(replaceMarkers,5000,coord,model)
 }
