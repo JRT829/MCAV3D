d678478eae37e0b5ce83c50239c4b3ac6e73877c
Jin_Tay <jtay0051@student.monash.edu>
2023-02-21 21:20:46 +1100

    object orientation

--- ptnswapi.py
+++ ptnswapi.py
@@ -13,19 +13,26 @@ responsebody=resp.content
 #Converting protobuf format into object for manipulation 
 feed = gtfs_realtime_pb2.FeedMessage()
 feed.ParseFromString(responsebody)
-#print(feed.entity) 
+
 print(feed)
-#Retrieving coordinates 
-latitude=[]
-longitude=[]
-routeid=[]
+
+class tram:
+   def __init__(self,latitude,longitude,routeid):
+      self.latitude=latitude
+      self.longitude=longitude
+      self.routeid=routeid
+
+tramlist=[]
 for entity in feed.entity:
-   latitude.append(entity.vehicle.position.latitude)
-   longitude.append(entity.vehicle.position.longitude)
-   routeid.append(entity.vehicle.trip.route_id)
+   latitudetemp=entity.vehicle.position.latitude
+   longitudetemp=entity.vehicle.position.longitude
+   routeidtemp= entity.vehicle.trip.route_id
+   tramlist.append(tram(latitudetemp,longitudetemp,routeidtemp))
 
-print(routeid)
 
+print(tramlist)
+
+#reverse geolocation
 requesturl='https://api.transport.nsw.gov.au/v1/tp/stop_finder?outputFormat=rapidJSON&type_sf=stop&name_sf='+'10101100'+'&coordOutputFormat=EPSG%3A4326&TfNSWSF=true&version=10.2.1.42'
 req = requests.get(requesturl,headers=headers)
 reqcontent=req.content
